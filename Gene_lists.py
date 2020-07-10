a289834318969beb17a6a31122105fcbe3ab88f9
Lea-Hein <lea.hein@student.uni-tuebingen.de>
2020-07-10 11:38:51 +0200

    contains lists of homologous recombination genes and genes that can cause resistance to ICB

---
+++ Gene_lists.py
@@ -0,0 +1,23 @@
+
+global hrgenes_list
+global gene_list
+global resistance_genes_list
+
+
+hrgenes_list = [["ATM", 0,], ["BRCA1", 0,], ["BRCA2", 0,], ["CHECK2", 0,], ["ERCC3", 0,], ["FANCA", 0,], ["HDAC2", 0,], ["MLH3", 0,], ["MRE11", 0,], ["NBN", 0,], ["PALB2", 0,], ["RAD51", 0,], ["ATR", 0,], ["ERCC6", 0,], ["FANCC", 0,], ["FANCG", 0,], ["FANCD2", 0,], ["PTEN", 0,], ["RAD18", 0,],["RAD51B", 0,], ["Rd54l", 0,], ["RPA1", 0,], ["XRCC3", 0,]]
+
+
+hrgenes_list_cnv = [["ATM", 0,], ["BRCA1", 0,], ["BRCA2", 0,], ["CHECK2", 0,], ["ERCC3", 0,], ["FANCA", 0,], ["HDAC2", 0,], ["MLH3", 0,], ["MRE11", 0,], ["NBN", 0,], ["PALB2", 0,], ["RAD51", 0,], ["ATR", 0,], ["ERCC6", 0,], ["FANCC", 0,], ["FANCG", 0,], ["FANCD2", 0,], ["PTEN", 0,], ["RAD18", 0,],["RAD51B", 0,], ["Rd54l", 0,], ["RPA1", 0,], ["XRCC3", 0,]]
+
+	# Genes that can result in Resistance to ICB when altered:
+	# IFR == TRIM63  -> TRIM63 is the newer label for the gene
+resistance_genes_list = [["PTEN", 0], ["EGFR", 0], ["MYC", 0], ["JAK1", 0], ["JAK2",0], ["IFNGR1", 0], ["IFNGR2", 0], ["IRF", 0], ["TRIM63",0] ]
+
+
+	# Genes that can result in Resistance to ICB when altered:
+	# IFR == TRIM63  -> TRIM63 is the newer label for the gene
+resistance_genes_list_cnv = [["PTEN", 0], ["EGFR", 0], ["MYC", 0], ["JAK1", 0], ["JAK2",0], ["IFNGR1", 0], ["IFNGR2", 0], ["IRF", 0], ["TRIM63",0] ]
+
+gene_list = [["ATM", 0], ["BRCA1", 0], ["BRCA2", 0], ["CHECK2", 0], ["ERCC3", 0], ["FANCA", 0], ["HDAC2", 0], ["MLH3", 0], ["MRE11", 0], ["NBN", 0], ["PALB2", 0], ["RAD51", 0], ["ATR", 0], ["ERCC6", 0], ["FANCC", 0], ["FANCG", 0], ["FANCD2", 0], ["PTEN", 0], ["RAD18", 0],["RAD51B", 0], ["Rd54l", 0], ["RPA1", 0], ["XRCC3", 0]]
+
+
